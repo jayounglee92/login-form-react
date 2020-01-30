d1a75da721b9ad3dc249f2981ec4b74a06f35ea4
jayounglee92 <lzy6853@gmail.com>
2020-01-30 18:33:09 +0900

    5장 로그인, 회원가입 완성 리덕스 액션 확인

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Root from './Root';
-import * as serviceWorker from './serviceWorker';
-import configureStore from './redux/configureStore';
+import Root from 'Root';
+import 'index.css';
+import configureStore from 'redux/configureStore';
+import { AppContainer } from 'react-hot-loader';
+import * as serviceWorker from 'serviceWorker';
 
 const store = configureStore();
 
-ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component store={store}/>
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
+render(Root);
+
+if(module.hot) {
+    module.hot.accept('./Root', () => render(Root));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,6 +4,14 @@ import modules from './modules';
 // TODO: 미들웨어, react-hot-loader 적용
 const configureStore = (initialState) => {
     const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+    // hot-reloading 를 위한 코드
+    if(module.hot) {
+        module.hot.accept('./modules', () => {
+            const nextRootReducer = require('./modules').default;
+            store.replaceReducer(nextRootReducer);
+        });
+    }
     return store;
 }
 
