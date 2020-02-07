aed970c05b1a7425473e2bbf6385dacd9f88aa41
jayounglee92 <lzy6853@gmail.com>
2020-02-07 18:07:53 +0900

    6장 로컬스토리지 헬퍼 작성하기 ing

--- src/containers/Auth/Register.js
+++ src/containers/Auth/Register.js
@@ -1,16 +1,124 @@
 import React, { Component } from 'react';
-import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink } from 'components/Auth';
+import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink, AuthError } from 'components/Auth';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as authActions from 'redux/modules/auth';
+import { isEmail, isLength, isAlphanumeric } from 'validator';
+import debounce from 'lodash/debounce';
 
 
 class Register extends Component {
+
 
     componentWillUnmount() {
         const { AuthActions } = this.props;
         AuthActions.initializeForm('register')
     }
+
+    setError = (message) => {
+        const { AuthActions } = this.props;
+        AuthActions.setError({
+            form: 'register',
+            message
+        });
+    }
+
+    validate = {
+        email: (value) => {
+            if(!isEmail(value)) {
+                this.setError('잘못된 이메일 형식입니다.');
+                return false;
+            }
+            return true;
+        },
+        username: (value) => {
+            if(!isAlphanumeric(value) || !isLength(value, { min:4, max: 15 })) {
+                this.setError('아이디는 4~15 글자의 알파벳 혹은 숫자로 이뤄져야 합니다.');
+                return false;
+            }
+            return true;
+        },
+        password: (value) => {
+            if(!isLength(value, { min: 6 })) {
+                this.setError('비밀번호를 6자 이상 입력하세요.');
+                return false;
+            }
+            this.setError(null); // 이메일과 아이디는 에러 null 처리를 중복확인 부분에서 하게 됩니다
+            return true;
+        },
+        passwordConfirm: (value) => {
+            if(this.props.form.get('password') !== value) {
+                this.setError('비밀번호확인이 일치하지 않습니다.');
+                return false;
+            }
+            this.setError(null);
+            return true;
+        }
+    }
+
+    checkEmailExists = debounce(async (email) => {
+        const { AuthActions } = this.props;
+        try {
+            await AuthActions.checkEmailExists(email);
+            if(this.props.exists.get('email')) {
+                this.setError('이미 존재하는 이메일입니다.');
+            } else {
+                this.setError(null);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }, 300)
+    
+    checkUsernameExists = debounce(async (username) => {
+        const { AuthActions } = this.props;
+        try {
+            await AuthActions.checkUsernameExists(username);
+            if(this.props.exists.get('username')) {
+                this.setError('이미 존재하는 아이디입니다.');
+            } else {
+                this.setError(null);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+        
+    }, 300)
+
+    handleLocalRegister = async () => {
+        const { form, AuthActions, error, history } = this.props;
+        const { email, username, password, passwordConfirm } = form.toJS();
+
+        const { validate } = this;
+
+        if(error) return; // 현재 에러가 있는 상태라면 진행하지 않음
+        if(!validate['email'](email) 
+            || !validate['username'](username) 
+            || !validate['password'](password) 
+            || !validate['passwordConfirm'](passwordConfirm)) { 
+            // 하나라도 실패하면 진행하지 않음
+            return;
+        }
+
+        try {
+            await AuthActions.localRegister({
+                email, username, password
+            });
+            const loggedInfo = this.props.result.toJS();
+            console.log(loggedInfo);
+            // TODO: 로그인 정보 저장 (로컬스토리지/스토어)
+            history.push('/'); // 회원가입 성공시 홈페이지로 이동
+        } catch(e) {
+            // 에러 처리하기
+            if(e.response.status === 409) {
+                const { key } = e.response.data;
+                const message = key === 'email' ? '이미 존재하는 이메일입니다.' : '이미 존재하는 아이디입니다.';
+                return this.setError(message);
+            }
+            this.setError('알 수 없는 에러가 발생했습니다.')
+        }
+    }
+
 
     handleChange = (e) => {
         const { AuthActions } = this.props;
@@ -21,11 +129,18 @@ class Register extends Component {
             value,
             form: 'register'
         });
+
+        // 검증작업 진행
+        const validation = this.validate[name](value);
+        if(name.indexOf('password') > -1 || !validation) return; // 비밀번호 검증이거나, 검증 실패하면 여기서 마침
     }
 
+    // TODO: 이메일, 아이디 중복 확인
+
     render() {
+        const { error } = this.props;
         const { email, username, password, passwordConfirm } = this.props.form.toJS();
-        const { handleChange } = this;
+        const { handleChange, handleLocalRegister } = this;
 
         return (
             <AuthContent title="회원가입">
@@ -59,7 +174,10 @@ class Register extends Component {
                     value={passwordConfirm}
                     onChange={handleChange}
                 />
-                <AuthButton>회원가입</AuthButton>
+                {
+                    error && <AuthError>{error}</AuthError>
+                }
+                <AuthButton onClick={handleLocalRegister}>회원가입</AuthButton>
                 <RightAlignedLink to="/auth/login">로그인</RightAlignedLink>
             </AuthContent>
         );
@@ -68,7 +186,10 @@ class Register extends Component {
 
 export default connect(
     (state) => ({
-        form: state.auth.getIn(['register', 'form'])
+        form: state.auth.getIn(['register', 'form']),
+        error: state.auth.getIn(['register', 'error']),
+        exists: state.auth.getIn(['register', 'exists']),
+        result: state.auth.get('result')
     }),
     (dispatch) => ({
         AuthActions: bindActionCreators(authActions, dispatch)
